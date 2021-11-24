f16dac7fc31eaeb8aa9b849cfd167375a3c112a2
overult01 <overult01@gmail.com>
2021-11-24 13:48:03 +0900

    회원가입(join) 기능 테스트 추가 // 순수 자바로만 구성(DIP 위반 상태)

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,6 +1,6 @@
 package hello.core.member;
 
-public class MemberServiceIml implements MemberService{
+public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
