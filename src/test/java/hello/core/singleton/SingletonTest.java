58f750e436cf712d4ba97fa68e232ffad359cd8f
overult01 <overult01@gmail.com>
2021-11-30 21:42:05 +0900

    싱글톤 패턴(싱글톤 컨테이너 사용 전): 클래스의 인스턴스가 딱 1개만 생성되는 것을 보장하는 디자인 패턴

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SingletonTest {
 
     @Test
@@ -23,7 +25,24 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         // memberService1 !== memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+//        new SingletonService();  <- 당연히 private access땜에 안됨
+
+        // 호출할 때마다 같은 객체를 반환(자바가 뜰 때, 미리 생성되었던 객체를 가져오기만 함)
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        // 같은 객체 인스턴스가 반환됨
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+
     }
 
 }
