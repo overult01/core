582fb14fe0c67562196b435ac70d3c9a27cf907d
overult01 <overult01@gmail.com>
2021-12-06 21:38:37 +0900

    컴포넌트스캔에서의 탐색위치(basePackages), 시작위치 지정(권장: 설정 정보의 클래스를 프로젝트 최상단에 위치)

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.FilterType;
 // 보통 설정 정보(AppConfig)를 컴포넌트 스캔 대상에서 제외하지는 않지만, 기존 예제 코드를 최대한 유지하기 위해서 제외해줌
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.core.member", -> 탐색할 패키지의 시작 위치를 지정
+//        basePackageClasses = AutoAppConfig.class, -> 지정한 클래스의 패키지를 탐색 시작 위치로 지정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
