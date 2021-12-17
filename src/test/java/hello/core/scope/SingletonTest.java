820249c368dcaa951676a764e52e425983c519c6
overult01 <overult01@gmail.com>
2021-12-17 22:15:10 +0900

    빈 스코프 종류 2: 프로토타입 스코프 (1은 싱글톤)

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,41 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+// 싱글톤 스코프
+    @Test
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        assertThat(singletonBean1).isSameAs(singletonBean2);
+
+        ac.close(); // 종료
+    }
+
+    @Scope("singleton") // 원래는 디폴트이기 때문에 생략해도 무방
+    static  class SingletonBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+
+    }
+}
